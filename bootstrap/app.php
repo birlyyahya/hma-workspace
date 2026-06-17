02099ab3f8bc5b10690c82881d45dd121ff8c86a
birlyyahya <birlypecker123@gmail.com>
2026-06-17 14:38:29 +0700

    fix trusted proxies for reverse proxy deployment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,8 @@
 <?php
-
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;                                  // <-- TAMBAH
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,6 +14,14 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'mobile' => \App\Http\Middleware\EnsureMobileDevice::class,
         ]);
+
+        // Cara A: percayai header X-Forwarded-* dari nginx (port/proto/host asli)
+        $middleware->trustProxies(at: '*', headers:
+            Request::HEADER_X_FORWARDED_FOR |
+            Request::HEADER_X_FORWARDED_HOST |
+            Request::HEADER_X_FORWARDED_PORT |
+            Request::HEADER_X_FORWARDED_PROTO
+        );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
